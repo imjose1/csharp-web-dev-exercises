e39a9f19076624ae22882aaac973a45f14f709ea
Jose g hernandez <jose_ghj@hotmail.com>
2023-03-19 12:41:49 -0500

    done

--- IceCreamShop/FlavorComparer.cs
+++ IceCreamShop/FlavorComparer.cs
@@ -8,11 +8,11 @@ namespace IceCreamShop
 {
     public class FlavorComparer : IComparer<Flavor>
     {
-        public int Compare(Flavor x, Flavor y)
-        {
-            return string.Compare(x.Name, y.Name);
-        }
+        //public int Compare(Flavor x, Flavor y)
+        //{
+        //    return string.Compare(x.Name, y.Name);
+        //}
 
-
+        
     }
 }
